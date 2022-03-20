1a210fbfcb9d185b23ba86c0862adadc423295a5
eraycode01 <eraycode01@gmail.com>
2022-03-20 19:40:07 +0300

    deneme commiti

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>zafer</title>
+        <title>Online Survey</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
